240ac4f92352614e86033bd85d3d4f499fbd5f6c
xerneas3318 <xerneas3318@users.noreply.github.com>
2026-06-12 11:59:01 -0400

    fix sha256 for hackfetch v0.1.1

--- Formula/hackfetch.rb
+++ Formula/hackfetch.rb
@@ -2,7 +2,7 @@ class Hackfetch < Formula
   desc "Hack Club themed system fetch with Hackatime stats"
   homepage "https://github.com/xerneas3318/hackfetch"
   url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "3d06109fc579999b17650b4d8ea979a45ab9d9877bb62575c7554ed020c84f73"
   license "MIT"
   head "https://github.com/xerneas3318/hackfetch.git", branch: "main"
 
