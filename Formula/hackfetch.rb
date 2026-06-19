3a6ebb96da8e866c3a96d1bafc7742968a2deadd
xerneas3318 <xerneas3318@users.noreply.github.com>
2026-06-19 18:05:28 -0400

    bump hackfetch to v1.3.0

--- Formula/hackfetch.rb
+++ Formula/hackfetch.rb
@@ -1,8 +1,8 @@
 class Hackfetch < Formula
   desc "Hack Club themed system fetch with Hackatime stats"
   homepage "https://github.com/xerneas3318/hackfetch"
-  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "2ba9c4f600543898cec3215eeb17127380c1044aa93f57615136c1c3570e61d7"
+  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "dd1e05bea76f848cf272670ea851defbb3acc6198157e07d2f31a01b4adc90f2"
   license "MIT"
   head "https://github.com/xerneas3318/hackfetch.git", branch: "main"
 
