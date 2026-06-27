8082a2aa384aa8bcc4ec8093c2825e4d6ba037ed
xerneas3318 <184197860+xerneas3318@users.noreply.github.com>
2026-06-27 16:46:37 -0700

    neurozip v0.2.0-beta.2

--- Formula/neurozip.rb
+++ Formula/neurozip.rb
@@ -3,8 +3,8 @@ class Neurozip < Formula
 
   desc "Task-aware neural compression for brain/biomedical signals"
   homepage "https://github.com/xerneas3318/NeuroZip"
-  url "https://github.com/xerneas3318/NeuroZip/releases/download/v0.2.0-beta.1/neurozip-0.2.0-beta.1.tar.gz"
-  sha256 "dd17c4da8608f24e847ed1151ecdb787e6937431d080bef71c378d388f2556e4"
+  url "https://github.com/xerneas3318/NeuroZip/releases/download/v0.2.0-beta.2/neurozip-0.2.0-beta.2.tar.gz"
+  sha256 "a25cbac69ab1d6ad65fd7935cc6d94650bfd613246560eb36ee1da1ae4f6fb5f"
   license "PolyForm-Noncommercial-1.0.0"
   head "https://github.com/xerneas3318/NeuroZip.git", branch: "model-testing"
 
