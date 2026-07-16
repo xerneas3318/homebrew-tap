2b34eb22e15d339c644e30a6789cd3227e6f0641
xerneas3318 <184197860+xerneas3318@users.noreply.github.com>
2026-07-16 13:59:57 +0530

    bump hackfetch to v1.6.0 (windows binaries + png/jpg export + polyform nc license)

--- Formula/hackfetch.rb
+++ Formula/hackfetch.rb
@@ -1,9 +1,9 @@
 class Hackfetch < Formula
   desc "Hack Club themed system fetch with Hackatime stats"
   homepage "https://github.com/xerneas3318/hackfetch"
-  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "5fa4d3b277e1abcef3ba84e523f0ba9628e87170e3dbb4fcb422e4a97d87434a"
-  license "MIT"
+  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.6.0.tar.gz"
+  sha256 "8eb80ade37e6a138440a934b67b3fd5e6a7b8bcbfbe069d59218c21e6b9c1d77"
+  license "PolyForm-Noncommercial-1.0.0"
   head "https://github.com/xerneas3318/hackfetch.git", branch: "main"
 
   depends_on "go" => :build
