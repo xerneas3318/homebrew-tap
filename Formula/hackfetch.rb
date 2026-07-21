aa70aa641f37ae46fef6f3acfc32737b7572b836
xerneas3318 <184197860+xerneas3318@users.noreply.github.com>
2026-07-21 19:50:09 +0530

    bump hackfetch to v1.10.1 (streak fix)

--- Formula/hackfetch.rb
+++ Formula/hackfetch.rb
@@ -1,8 +1,8 @@
 class Hackfetch < Formula
   desc "Hack Club themed system fetch with Hackatime stats"
   homepage "https://github.com/xerneas3318/hackfetch"
-  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.10.0.tar.gz"
-  sha256 "080799983d84807074fabee65a897d1af95e96d102da565d98ce76514cb19fb9"
+  url "https://github.com/xerneas3318/hackfetch/archive/refs/tags/v1.10.1.tar.gz"
+  sha256 "173b3274f4544ab7c9b812f750b3d7dd3bb3cbb8935665038e6da8ce37ed2ea1"
   license "PolyForm-Noncommercial-1.0.0"
   head "https://github.com/xerneas3318/hackfetch.git", branch: "main"
 
